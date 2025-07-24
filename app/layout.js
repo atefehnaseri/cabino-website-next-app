dc50ae177e83aaeac698d2e092d569c36959d24c
Atefeh Naseri <ati_nas@yahoo.com>
2025-07-24 15:35:20 +0200

    chore(layout): add header component and layout styles

--- app/layout.js
+++ app/layout.js
@@ -9,6 +9,7 @@ const josefin = Josefin_Sans({
 });
 
 import "@/app/_styles/globals.css";
+import Header from "./_components/Header";
 
 export const metadata = {
   title: {
@@ -22,14 +23,13 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen `}
+        className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen flex flex-col`}
       >
-        <header>
-          <Logo />
-        </header>
+        <Header />
         <Navigation />
-        <main>{children}</main>
-        <footer>Copyright © 2023 Cabino Agency</footer>
+        <div className="flex-1 px-8 py-12">
+          <main className="max-w-7xl mx-auto">{children}</main>
+        </div>
       </body>
     </html>
   );
