0674ece5f4ea17ce7c6c96c80382e54d7e610ea6
Atefeh Naseri <ati_nas@yahoo.com>
2025-07-24 14:43:12 +0200

    chore(font): add global font to the body

--- app/layout.js
+++ app/layout.js
@@ -1,16 +1,29 @@
 import Logo from "./_components/Logo";
 import Navigation from "./_components/Navigation";
+
+import { Josefin_Sans } from "next/font/google";
+
+const josefin = Josefin_Sans({
+  subsets: ["latin"],
+  display: "swap",
+});
+
 import "@/app/_styles/globals.css";
 
 export const metadata = {
-  title: "Cabino Agency",
-  description: "Cabino agency website",
+  title: {
+    default: "Cabino Agency",
+    template: "%s  Cabino Agency",
+  },
+  description: "Cabino agency website, located in the heart of the city.",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body
+        className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen `}
+      >
         <header>
           <Logo />
         </header>
