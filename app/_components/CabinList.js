00c8ae7a229cdb517c1fdabd05b743ea1c00ea7e
Atefeh Naseri <ati_nas@yahoo.com>
2025-07-31 18:32:14 +0200

    chore(caching): set up some caching approach, add production set up and out folder

--- app/_components/CabinList.js
+++ app/_components/CabinList.js
@@ -4,6 +4,7 @@ import CabinCard from "@/app/_components/CabinCard";
 import { getCabins } from "@/app/_lib/data-service";
 
 async function CabinList() {
+  // noStore();
   const cabins = await getCabins();
 
   if (!cabins.length) return null;
--- next.config.mjs
+++ next.config.mjs
@@ -10,7 +10,13 @@ const nextConfig = {
         search: "",
       },
     ],
+    // Image Optimization through next/image can be used with a static export by defining a custom image loader in next.config.js. For example, you can optimize images with a service like Cloudinary:
+
+    // loader: "custom",
+    // loaderFile: "./my-loader.ts",
   },
+  // exporting as a SSG
+  // output: "export",
 };
 
 export default nextConfig;
