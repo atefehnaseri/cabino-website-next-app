59e5d81812126706f59e0f8fb2994b81927f9aa0
Atefeh Naseri <ati_nas@yahoo.com>
2025-08-04 14:44:59 +0200

    feat(cabinsListFilter): add filter client component and add it to the cabins page

---
+++ app/_components/Filter.js
@@ -0,0 +1,73 @@
+"use client";
+
+import { useRouter, usePathname, useSearchParams } from "next/navigation";
+import { useCallback, useEffect, useState } from "react";
+
+const filterOptions = [
+  {
+    label: "All cabins",
+    value: "all",
+  },
+  {
+    label: "2-3 guests",
+    value: "small",
+  },
+  {
+    label: "4-7 guests",
+    value: "medium",
+  },
+  {
+    label: "8-12 guests",
+    value: "large",
+  },
+];
+export default function Filter() {
+  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const activeFilter = searchParams.get("capacity") ?? "all";
+
+  const createQueryString = (name, value) => {
+    const params = new URLSearchParams(searchParams.toString());
+    params.set(name, value);
+
+    return params.toString();
+  };
+
+  function handleFilter(filter) {
+    console.log(filter);
+    router.replace(
+      `${pathname + "?" + createQueryString("capacity", filter)}`,
+      { scroll: false }
+    );
+  }
+
+  return (
+    <div className="border border-primary-800 flex">
+      {filterOptions.map((filter) => (
+        <FilterButton
+          key={filter.value}
+          filter={filter.value}
+          handleFilter={handleFilter}
+          activeFilter={activeFilter}
+        >
+          {filter.label}
+        </FilterButton>
+      ))}
+    </div>
+  );
+}
+
+function FilterButton({ filter, handleFilter, activeFilter, children }) {
+  return (
+    <button
+      type="button"
+      className={`px-5 py-2 hover:bg-primary-700 ${
+        filter === activeFilter ? "bg-primary-700 text-primary-50" : ""
+      }`}
+      onClick={() => handleFilter(filter)}
+    >
+      {children}
+    </button>
+  );
+}
--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,6 +1,7 @@
 import { Suspense } from "react";
 import CabinList from "@/app/_components/CabinList";
 import Spinner from "@/app/_components/Spinner";
+import Filter from "../_components/Filter";
 
 export const revalidate = 3600;
 // export const revalidate = 15;
@@ -9,7 +10,8 @@ export const metadata = {
   title: "Cabins",
 };
 
-export default async function page() {
+export default function page({ searchParams }) {
+  const filter = searchParams?.capacity ?? "all";
   return (
     <div>
       <h1 className="text-4xl mb-5 text-accent-400 font-medium">
@@ -24,8 +26,12 @@ export default async function page() {
         Welcome to paradise.
       </p>
 
-      <Suspense fallback={<Spinner />}>
-        <CabinList />
+      <div className="flex justify-end mb-8">
+        <Filter />
+      </div>
+
+      <Suspense fallback={<Spinner />} key={filter}>
+        <CabinList filter={filter} />
       </Suspense>
     </div>
   );
