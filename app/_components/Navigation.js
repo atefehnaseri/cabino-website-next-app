545f55236f757a2c49c1ba788ce981fabf31134d
Atefeh Naseri <ati_nas@yahoo.com>
2025-08-11 16:33:40 +0200

    chor(auth): add next auth for authentication to the project

--- app/_components/Navigation.js
+++ app/_components/Navigation.js
@@ -1,6 +1,8 @@
 import Link from "next/link";
+import { auth } from "@/app/_lib/auth";
 
-export default function Navigation() {
+export default async function Navigation() {
+  const session = await auth();
   return (
     <nav className="z-10 text-xl">
       <ul className="flex gap-16 items-center">
@@ -23,9 +25,17 @@ export default function Navigation() {
         <li>
           <Link
             href="/account"
-            className="hover:text-accent-400 transition-colors"
+            className="hover:text-accent-400 transition-colors flex items-center gap-4"
           >
-            Guest area
+            {session?.user?.image && (
+              <img
+                src={session.user.image}
+                alt={session.user.name}
+                className="h-8 rounded-full"
+                referrerPolicy="no-referrer"
+              />
+            )}
+            <span>Guest area</span>
           </Link>
         </li>
       </ul>
--- app/_components/ReservationForm.js
+++ app/_components/ReservationForm.js
@@ -2,7 +2,7 @@
 
 import { useReservationContext } from "./ReservationContext";
 
-function ReservationForm({ cabin }) {
+function ReservationForm({ cabin, user }) {
   const { dateRange } = useReservationContext();
 
   const { maxCapacity } = cabin;
@@ -12,16 +12,16 @@ function ReservationForm({ cabin }) {
       <div className="bg-primary-800 text-primary-300 px-16 py-2 flex justify-between items-center h-auto">
         <p>Logged in as</p>
 
-        {/* <div className='flex gap-4 items-center'>
+        <div className="flex gap-4 items-center">
           <img
             // Important to display google profile images
-            referrerPolicy='no-referrer'
-            className='h-8 rounded-full'
+            referrerPolicy="no-referrer"
+            className="h-8 rounded-full"
             src={user.image}
             alt={user.name}
           />
           <p>{user.name}</p>
-        </div> */}
+        </div>
       </div>
 
       <form className="bg-primary-900 py-10 px-16 text-lg flex gap-5 flex-col">
@@ -64,9 +64,6 @@ function ReservationForm({ cabin }) {
           </button>
         </div>
       </form>
-      <p>
-        {String(dateRange.from)} {String(dateRange.to)}
-      </p>
     </div>
   );
 }
