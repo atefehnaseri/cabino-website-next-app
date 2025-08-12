9e9ab9612030f6a1a5fb82a17a8ae65230482fac
Atefeh Naseri <ati_nas@yahoo.com>
2025-08-12 16:01:46 +0200

    feat(updateGuestProfile): add server action for updating guest profile and solve the caching problem within the update form

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -1,6 +1,8 @@
 "use server";
 
-import { signIn, signOut } from "./auth";
+import { revalidatePath } from "next/cache";
+import { auth, signIn, signOut } from "./auth";
+import { supabase } from "./supabase";
 
 export async function signInAction() {
   return await signIn("google", { redirectTo: "/account" });
@@ -8,4 +10,32 @@ export async function signInAction() {
 
 export async function signOutAction() {
   return await signOut({ redirectTo: "/" });
+}
+
+export async function updateGuestProfileAction(formData) {
+  const session = await auth();
+  if (!session) throw new Error("You must be logged into update your profile!");
+
+  const [nationality, countryFlag] = formData.get("nationality").split("%");
+
+  const nationalID = formData.get("nationalID");
+  //check the national Id validation
+  if (!/^[a-zA-Z0-9]{6,12}$/.test(nationalID))
+    throw new Error("Please provide a valid national ID");
+
+  const updatedData = { nationality, countryFlag, nationalID };
+  const { data, error } = await supabase
+    .from("guests")
+    .update(updatedData)
+    .eq("id", session.user.guestId)
+    .select()
+    .single();
+
+  if (error) {
+    console.error(error);
+    throw new Error("Guest could not be updated");
+  }
+
+  //revalidate the page
+  revalidatePath("/account/profile");
 }
