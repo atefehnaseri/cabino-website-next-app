4da096e7b76423e2855a6cfee99bd906a48c9aef
Atefeh Naseri <ati_nas@yahoo.com>
2025-08-13 09:32:51 +0200

    feat(deleteReservation): use useTransition hook to delete a reservation by clicking on delete button

--- app/_components/DeleteReservation.js
+++ app/_components/DeleteReservation.js
@@ -1,19 +1,33 @@
 "use client";
 
+import { useTransition } from "react";
 import { TrashIcon } from "@heroicons/react/24/solid";
-import { useFormStatus } from "react-dom";
 import { deleteReservation } from "@/app/_lib/actions";
+import SpinnerMini from "./SpinnerMini";
 
 function DeleteReservation({ bookingId }) {
-  const { pending } = useFormStatus();
+  const [isPending, startTransition] = useTransition();
+
+  function handleDeleteReservation() {
+    if (confirm("Are you sure you want to delete this reservation?"))
+      startTransition(() => deleteReservation(bookingId));
+  }
   return (
     <button
-      onClick={() => deleteReservation(bookingId)}
+      onClick={handleDeleteReservation}
       className="group flex items-center gap-2 uppercase text-xs font-bold text-primary-300 flex-grow px-3 hover:bg-accent-600 transition-colors hover:text-primary-900"
-      disabled={pending}
+      disabled={isPending}
     >
-      <TrashIcon className="h-5 w-5 text-primary-600 group-hover:text-primary-800 transition-colors" />
-      <span className="mt-1">{pending ? "Deleting.." : "Delete"}</span>
+      {!isPending ? (
+        <>
+          <TrashIcon className="h-5 w-5 text-primary-600 group-hover:text-primary-800 transition-colors" />
+          <span className="mt-1">Delete</span>
+        </>
+      ) : (
+        <span className="mx-auto">
+          <SpinnerMini />
+        </span>
+      )}
     </button>
   );
 }
