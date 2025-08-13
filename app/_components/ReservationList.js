44f0e73d70133c20fc93b2b0fd4e1c3d0a0601e1
Atefeh Naseri <ati_nas@yahoo.com>
2025-08-13 13:06:55 +0200

    feat(deleteReservation): delete reservation immediately by useOptimistic hook

---
+++ app/_components/ReservationList.js
@@ -0,0 +1,35 @@
+"use client";
+
+import { useOptimistic } from "react";
+import ReservationCard from "./ReservationCard";
+import { deleteReservation } from "@/app/_lib/actions";
+
+function ReservationList({ bookings }) {
+  const [optimisticBookings, optimisticDelete] = useOptimistic(
+    bookings,
+    (currentOptimisticBookings, bookingId) => {
+      return currentOptimisticBookings.filter(
+        (booking) => booking.id !== bookingId
+      );
+    }
+  );
+
+  async function handleDeleteReservation(bookingId) {
+    optimisticDelete(bookingId);
+    await deleteReservation(bookingId);
+  }
+
+  return (
+    <ul className="space-y-6">
+      {optimisticBookings.map((booking) => (
+        <ReservationCard
+          booking={booking}
+          key={booking.id}
+          onDelete={handleDeleteReservation}
+        />
+      ))}
+    </ul>
+  );
+}
+
+export default ReservationList;
